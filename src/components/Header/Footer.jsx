59eb78c7f3b109831c4d464f4d30b11e8a7787fc
unknown <samshyam9630@gmail.com>
2023-07-30 09:19:40 +0530

    responsive and working

--- src/components/Header/Footer.jsx
+++ src/components/Header/Footer.jsx
@@ -2,14 +2,26 @@ import React from 'react'
 import { BsGithub, BsInstagram, BsLinkedin, BsTwitter } from 'react-icons/bs';
 
 const Footer = () => {
+const github = () => {
+  window.open("https://github.com/shyamg090", "_blank");
+};
+const linkedin = () => {
+  window.open("https://www.linkedin.com/in/shyam-g-6712a522b", "_blank");
+};
+const twitter = () => {
+  window.open("https://twitter.com/_Shyam_G", "_blank");
+};
+const instagram = () => {
+  window.open("https://www.instagram.com/shyam_g__/", "_blank");
+};
 
   return (
     <div className="footer">
       <div className="links-logo">
-        <BsGithub href="https://github.com/shyamg090"  target='_blank' />
-        <BsLinkedin href="https://www.linkedin.com/in/shyam-g-6712a522b" target='_blank'  />
-        <BsTwitter href="https://twitter.com/_Shyam_G"  target='_blank' />
-        <BsInstagram href="https://www.instagram.com/shyam_g__/"  target='_blank' />
+        <BsGithub onClick={()=>github()} />
+        <BsLinkedin onClick={()=> linkedin()}  />
+        <BsTwitter onClick={()=> twitter()} />
+        <BsInstagram onClick={()=> instagram()} />
       </div>
       <div className="about">copyright © Shyam G</div>
     </div>
